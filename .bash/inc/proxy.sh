ee9ca88783713f14b2ef7c1d543299f3f0af97e3
changj2 <sam.chang@cba.com.au>
2020-07-23 19:26:33 +1000

    Backup settings

--- .bash/inc/proxy.sh
+++ .bash/inc/proxy.sh
@@ -7,6 +7,22 @@ proxy_print() {
     echo no_proxy=$no_proxy
 }
 
+proxy_config_edit() {
+    code /usr/local/etc/cntlm.conf
+}
+
+proxy_config_print() {
+    cat /usr/local/etc/cntlm.conf
+}
+
+proxy_verify() {
+    cntlm -v -M http://google.com
+}
+
+proxy_foreground() {
+    cntlm -v -f
+}
+
 proxy_status() {
     brew services list | grep --color=never 'Name\|cntlm'
 }
@@ -21,12 +37,17 @@ proxy_start() {
     proxy_status
 }
 
+proxy_restart() {
+    proxy_stop
+    proxy_start
+}
+
 proxy_unset() {
     unset NO_PROXY ALL_PROXY HTTP_PROXY HTTPS_PROXY no_proxy all_proxy http_proxy https_proxy
     proxy_print
 }
 
-proxy_enable() {
+proxy_set() {
     export NO_PROXY=$BASH_IT_NO_PROXY
     export ALL_PROXY=$BASH_IT_HTTP_PROXY
     export HTTP_PROXY=$BASH_IT_HTTP_PROXY
@@ -36,14 +57,19 @@ proxy_enable() {
     export all_proxy=$BASH_IT_HTTP_PROXY
     export http_proxy=$BASH_IT_HTTP_PROXY
     export https_proxy=$BASH_IT_HTTPS_PROXY
+}
 
+proxy_enable() {
+    proxy_set
     if [ ! -e $HOME/.proxy_enable ]; then
         if [ -n "$BASH_VERSION" ]; then
             bash-it enable plugin proxy
             enable-proxy
         fi
         if [ "$(uname)" = "Darwin" ]; then
-            networksetup -switchtolocation "Automatic" > /dev/null
+            # networksetup -switchtolocation "Automatic" > /dev/null
+            networksetup -setwebproxy Wi-Fi 127.0.0.1 3128 off
+            networksetup -setsecurewebproxy Wi-Fi 127.0.0.1 3128 off
         fi
         touch $HOME/.proxy_enable
     fi
@@ -58,7 +84,9 @@ proxy_disable() {
             disable-proxy
         fi
         if [ "$(uname)" = "Darwin" ]; then
-            networksetup -switchtolocation "No Proxy" > /dev/null
+            # networksetup -switchtolocation "No Proxy" > /dev/null
+            networksetup -setwebproxystate Wi-Fi off
+            networksetup -setsecurewebproxystate Wi-Fi off
         fi
     fi
 }
